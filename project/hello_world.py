b2768f70894e899c095b1ba263a4411f68d82a5b
aldaszarnauskas <aldas02027@gmail.com>
2026-03-27 20:53:27 +0200

    adding website template from geeksforgeeks and my initial input

--- project/hello_world.py
+++ project/hello_world.py
@@ -1,10 +1,80 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
-app = Flask(__name__, static_url_path="", static_folder="staticpahes")
+from dotenv import load_dotenv
+import os
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+load_dotenv()
 
-if __name__ == ("__main__") :
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')  
+app.config['MYSQL_DB'] = 'geeklogin'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def welcome():
+    return render_template("welcome.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            return render_template('index.html', msg='Logged in successfully!')
+        else:
+            msg = 'Incorrect username/password!'
+    return render_template('login.html', msg=msg)
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only letters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    return render_template('register.html', msg=msg)
+
+@app.route('/language_learning', methods=['GET', 'POST'])
+def learn_the_language():
+    msg = ''
+    return render_template('language_learning.html', msg=msg)
+
+if __name__ == '__main__':
     app.run(debug=True)
