098ce357d690dcf574a60c0e949dea2e5c3579fe
aldaszarnauskas <aldas02027@gmail.com>
2026-04-04 12:22:37 +0300

    replacing mysql with pythonsql

--- project/hello_world.py
+++ project/hello_world.py
@@ -1,48 +1,51 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
+from flask_sqlalchemy import SQLAlchemy
 import re
-
 from dotenv import load_dotenv
 import os
-
 from google import genai
+from get_a_dialogue import get_a_dialogue # Custom made app
 
-from get_a_dialogue import get_a_dialogue
 
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+db = SQLAlchemy(app)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')  
-app.config['MYSQL_DB'] = 'geeklogin'
+class Account(db.Model):
+    __tablename__ = 'accounts'   
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True
+                      )
+    
+with app.app_context():
+    db.create_all()
 
-mysql = MySQL(app)
+
+
+
 
 @app.route('/')
-def welcome():
-    return render_template("welcome.html")
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        username = request.form['username']
-        password = request.form['password']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
-        account = cursor.fetchone()
-        if account:
+        user = Account.query.filter_by(
+            username=request.form["username"],
+            password=request.form["password"]
+        ).first()
+        
+        if user:
             session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
+            session['id'] = user.id
+            session['username'] = user.username
             return render_template('index.html', msg='Logged in successfully!')
         else:
             msg = 'Incorrect username/password!'
-    return render_template('login.html', msg=msg)
+    return render_template('index.html', msg=msg)
 
 @app.route('/logout')
 def logout():
@@ -58,9 +61,10 @@ def register():
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-        account = cursor.fetchone()
+
+        # Check if username already exists
+        account = Account.query.filter_by(username=username).first()
+
         if account:
             msg = 'Account already exists!'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
@@ -70,9 +74,15 @@ def register():
         elif not username or not password or not email:
             msg = 'Please fill out the form!'
         else:
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email))
-            mysql.connection.commit()
+            new_account = Account(
+                username=username,
+                password=password,
+                email=email
+            )
+            db.session.add(new_account)
+            db.session.commit()
             msg = 'You have successfully registered!'
+
     return render_template('register.html', msg=msg)
 
 @app.route('/language_learning', methods=['GET', 'POST'])
